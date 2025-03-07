304e800e2690ba170ee2b515bfc619bbf26a3b44
Rodrigo Pontes Moreira <111535306+RpMoreira98@users.noreply.github.com>
2025-03-07 10:55:24 -0300

    fetch: (add page items)

---
+++ src/app/components/pageRegister/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export function PageRegister () {
+    return (
+       <div>
+        <header className="p-[40px] flex justify-center items-center">
+            <img src="/logo-full.svg" alt="Logo" className=""/>
+        </header>
+        <div>
+            <h1 className="pt-[20px] pl-[50px] pr-[50px] text-[60px]">Your Journey to Coding Conf 2025 Starts Here!</h1>
+        </div>
+       </div>
+    )
+}
