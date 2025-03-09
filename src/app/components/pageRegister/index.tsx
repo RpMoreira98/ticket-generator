b11f6985238c54b515d50eddfa5ce03cb90e81ae
Rodrigo Pontes Moreira <111535306+RpMoreira98@users.noreply.github.com>
2025-03-09 11:30:43 -0300

    fetch: (added the input to place the information)

--- src/app/components/pageRegister/index.tsx
+++ src/app/components/pageRegister/index.tsx
@@ -1,7 +1,31 @@
 "use client"
 
+import { useRef, useState } from "react"
 
 export function PageRegister () {
+    const [image, setImage] = useState<string | null>(null)
+    const fileInputRef = useRef<HTMLInputElement | null>(null)
+    const handleClickRef = () => {
+        fileInputRef.current?.click()
+    } 
+
+    const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if(!file) return;
+        if(file && (file.type === "image/jpg" || file.type === "image/png")) {
+            const reader = new FileReader(); 
+            reader.onload = (e) => {
+            const result = e.target?.result;
+            if(typeof result === "string") {
+                setImage(result)
+            }}
+            reader.readAsDataURL(file)
+        } else {
+            alert("Apenas arquivos JPEG, JPG e PNG!!")
+            setImage(null)
+        }
+        event.target.value = "";
+    }
     return (
        <div>
         <header className="p-[40px] flex justify-center items-center">
@@ -12,10 +36,44 @@ export function PageRegister () {
             <p className="pt-[50px] w-[100%] m-auto text-[22px] text-center text-[#d2d0d6]">Secure your spot at next year's biggest coding conference.</p>
             <form action="" className="flex flex-col w-[60%] mt-[40px] mr-auto ml-auto mb-0">
                 <label htmlFor="" className="w-[100%] m-auto">Upload Avatar</label>
-                <div className="flex flex-col justify-center items-center mb-[15px] mt-[10px] mr-0 ml-0 p-[20px] h-[140px] border-2 border-dashed border-[#4b486a] rounded-[6px] relative bg-[#4b486a4d] cursor-pointer">
-                    <input type="text" className="absolute w-[100%] h-[100%] opacity-0 cursor-pointer"/>
-                    <img src="/icon-upload.svg" alt="" className="w-[50px] h-[50px] border-1 border-solid border-[#8784a4] rounded-[10px] absolute top-[15%] transform-(-50%) bg-[#4b486a]"/>
+                <div onClick={handleClickRef} className="flex flex-col justify-center items-center mb-[15px] mt-[10px] mr-0 ml-0 p-[20px] h-[140px] border-2 border-dashed border-[#4b486a] rounded-[6px] relative bg-[#4b486a4d] cursor-pointer">
+                    <input accept="image/jpg, image/png" ref={fileInputRef} onChange={handleFileInput} type="file" className="hidden"/>
+                    {
+                     image ? (
+                        <img src={image} alt="Preview" className="w-[50px] h-[50px] border-1 border-solid border-[#8784a4] rounded-[10px] absolute top-[15%]"/>
+                     ) : (
+                        <img src="/icon-upload.svg" alt="" className="w-[50px] h-[50px] border-1 border-solid border-[#8784a4] rounded-[10px] absolute top-[15%] bg-[#4b486a]"/>
+                     )
+                    }
                     <p className="mt-[60px] text-white font-light">Drag and drop or click to upload</p>
+                </div>
+                <span className="flex text-[12px] items-center gap-2 text-[#8784a4]">
+                    <img src="/icon-info.svg" alt="" className=""/>
+                    Upload your photo (JPG or PNG, max size: 500KB).
+                </span>
+                <div className="flex flex-col mt-5">
+                    <label htmlFor="full-name" className="text-[20px] text-white">Full Name</label>
+                    <input type="text" name="name" className="block outline-none mt-[10px] ml-0 mb-[10px] mr-0 pt-[15px] pr-0 pb-[15px] pl-[10px] border border-solid border-[#8784a4] rounded-md text-[#d2d1d6] text-[15px] bg-[#4b486a4c] focus:border-[#ffffff] focus:ring-2 focus:ring-[#8784a4]"/>
+                    <span className="flex gap-1">
+                    <img src="/icon-info.svg" alt="" className=""/>
+                        Shouldn't be empty.
+                        </span>
+                </div>
+                <div className="flex flex-col mt-5">
+                    <label htmlFor="full-name" className="text-[20px] text-white">Email Address</label>
+                    <input type="text" name="name" className="block outline-none mt-[10px] ml-0 mb-[10px] mr-0 pt-[15px] pr-0 pb-[15px] pl-[10px] border border-solid border-[#8784a4] rounded-md text-[#d2d1d6] text-[15px] bg-[#4b486a4c] focus:border-[#ffffff] focus:ring-2 focus:ring-[#8784a4]"/>
+                    <span className="flex gap-1">
+                    <img src="/icon-info.svg" alt="" className=""/>
+                        Shouldn't be empty.
+                        </span>
+                </div>
+                <div className="flex flex-col mt-5">
+                    <label htmlFor="full-name" className="text-[20px] text-white">Github Username</label>
+                    <input type="text" name="name" className="outline-none mt-[10px] ml-0 mb-[10px] mr-0 pt-[15px] pr-0 pb-[15px] pl-[10px] border border-solid border-[#8784a4] rounded-md text-[#d2d1d6] text-[15px] bg-[#4b486a4c] focus:border-[#ffffff] focus:ring-2 focus:ring-[#8784a4]"/>
+                    <span className="flex gap-1">
+                    <img src="/icon-info.svg" alt="" className=""/>
+                        Shouldn't be empty.
+                        </span>
                 </div>
             </form>
         </div>
